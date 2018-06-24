346a4d889d8f4216ad729ee2f2b68eb32bb29bae
Mike's laptopper <mpdewildt@gmail.com>
2018-06-24 22:45:37 +0200

    werkende server en start databse connectie
    versie0.4

---
+++ IPASSWebServlet/src/IPASSServlet1.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IPASSServlet1
+ */
+@WebServlet("/IPASSServlet1")
+public class IPASSServlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final String HTML_START="<html><body>";
+	public static final String HTML_END="</body></html>";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IPASSServlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println(HTML_START + "<form action=\"UploadDownloadFileServlet\" method=\"post\" enctype=\"multipart/form-data\">\r\n" + 
+				"Selecteer GEDCOM File om te uploaden:<input type=\"file\" name=\"fileName\">\r\n" + 
+				"<br>\r\n" + 
+				"<input type=\"submit\" value=\"Upload\">"+HTML_END);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Ipass WebApp/src/IpassWebservlet1.java
@@ -0,0 +1,50 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IpassWebservlet1
+ */
+@WebServlet("/IpassWebservlet1")
+public class IpassWebservlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final String HTML_START="<html><body>";
+	public static final String HTML_END="</body></html>";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IpassWebservlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println(HTML_START + "<form action=\"UploadDownloadFileServlet\" method=\"post\" enctype=\"multipart/form-data\">\r\n" + 
+				"Selecteer GEDCOM File om te uploaden:<input type=\"file\" name=\"fileName\">\r\n" + 
+				"<br>\r\n" + 
+				"<input type=\"submit\" required accept=\".ged\" value=\"Upload\">"+HTML_END);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
+
+
